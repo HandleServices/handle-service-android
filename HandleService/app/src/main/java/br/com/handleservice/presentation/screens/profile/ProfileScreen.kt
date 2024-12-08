c38b83ea4d84bdddf57937836afd417f0a8bcd9a
unknown <netoelpidio1@gmail.com>
2024-12-07 21:28:52 -0300

    feature/formato inicial da tela de perfil

--- HandleService/app/src/main/java/br/com/handleservice/presentation/screens/profile/ProfileScreen.kt
+++ HandleService/app/src/main/java/br/com/handleservice/presentation/screens/profile/ProfileScreen.kt
@@ -8,15 +8,125 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import br.com.handleservice.R
+
 @Composable
 fun ProfileScreen(
-    modifier: Modifier = Modifier,
+    viewModel: ProfileViewModel = hiltViewModel()
 ) {
+    val profileState = viewModel.profileState.collectAsState().value
+
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.background))
+            .padding(horizontal = 16.dp),
     ) {
-        Text(text = "PROFILE PAGE")
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Profile Header
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.filled_person_icon),
+                contentDescription = "Foto de Perfil",
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = profileState.name,
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.Bold),
+                fontSize = 18.sp,
+                color = colorResource(R.color.handle_titles)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Options List
+        ProfileOption(
+            icon = R.drawable.ic_messages,
+            label = "Chat",
+            onClick = { /* Ação do Chat */ }
+        )
+        ProfileOption(
+            icon = R.drawable.ic_address,
+            label = "Meus Endereços",
+            onClick = { /* Ação dos Endereços */ }
+        )
+        ProfileOption(
+            icon = R.drawable.ic_document_data,
+            label = "Dados da Conta",
+            onClick = { /* Ação dos Dados da Conta */ }
+        )
+        ProfileOption(
+            icon = R.drawable.ic_settings,
+            label = "Configurações",
+            onClick = { /* Ação das Configurações */ }
+        )
     }
 }
+
+@Composable
+fun ProfileOption(
+    icon: Int,
+    label: String,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable(onClick = onClick)
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = label,
+            modifier = Modifier.size(24.dp),
+            tint = colorResource(R.color.handle_gray)
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Normal),
+            fontSize = 16.sp,
+            color = colorResource(R.color.handle_titles),
+            modifier = Modifier.weight(1f)
+        )
+
+        Icon(
+            painter = painterResource(R.drawable.ic_chevron_right),
+            contentDescription = "Seta",
+            modifier = Modifier.size(16.dp),
+            tint = colorResource(R.color.handle_gray)
+        )
+    }
+}
+
