f3b3b29371d31995ecd2dad7b22182fecea00da5
Gabriel Al-Samir <alsamir.gabriel@gmail.com>
2024-12-18 18:47:34 -0300

    wip

--- HandleService/app/src/main/java/br/com/handleservice/presentation/screens/worker/WorkerScreen.kt
+++ HandleService/app/src/main/java/br/com/handleservice/presentation/screens/worker/WorkerScreen.kt
@@ -1,4 +1,21 @@
 package br.com.handleservice.presentation.screens.worker
 
-class WorkerScreen {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import br.com.handleservice.presentation.screens.contracts.ContractsViewModel
+
+@Preview
+@Composable
+fun WorkerScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ContractsViewModel = hiltViewModel()
+) {
+    Column (
+
+    ) {
+
+    }
 }
