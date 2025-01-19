203def74c30c076a9fb69ba483476e35569874df
Dev-Luigy <luigygabriel18a@gmail.com>
2025-01-19 17:18:26 -0300

    refactor: Create a file for SimpleSearchCard and ServiceItem.

--- HandleService/app/src/main/java/br/com/handleservice/presentation/screens/simple_search/SimpleSearch.kt
+++ HandleService/app/src/main/java/br/com/handleservice/presentation/screens/simple_search/SimpleSearch.kt
@@ -32,20 +32,13 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.com.handleservice.R
 import br.com.handleservice.presentation.screens.simple_search.components.FilterButton
+import br.com.handleservice.presentation.screens.simple_search.components.ServiceItemCard
+import br.com.handleservice.presentation.screens.simple_search.model.ServiceItem
 import br.com.handleservice.ui.components.handleHeader.HandleHeader
 import br.com.handleservice.ui.components.searchbar.HandleSearchBar
 import br.com.handleservice.ui.mock.getMockWorker
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-
-data class ServiceItem(
-    val id: Int,
-    val name: String,
-    val rating: Double,
-    val category: String,
-    val isAvailableNow: Boolean,
-    val imageUrl: String
-)
 
 @Composable
 fun SearchScreen(query: String, navController: NavController?) {
@@ -144,101 +137,6 @@ fun SearchScreen(query: String, navController: NavController?) {
         }
     }
 }
-
-@Composable
-fun ServiceItemCard(item: ServiceItem, navController: NavController?) {
-    Card(
-        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
-        shape = RoundedCornerShape(8.dp),
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable {
-                println(item.id)
-                navController?.navigate("worker_screen/${item.id}")
-            }
-    ) {
-        Row(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(0.80f),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        color = MaterialTheme.colorScheme.onSurface,
-                        text = item.name,
-                        style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.W500),
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        fontSize = 12.sp,
-                        lineHeight = TextUnit(20F, TextUnitType.Sp)
-                    )
-
-                    if (item.isAvailableNow) {
-                        Text(
-                            text = "Disponível Agora!",
-                            style = MaterialTheme.typography.bodyMedium.copy(
-                                color = MaterialTheme.colorScheme.primary,
-                                fontWeight = FontWeight.Bold,
-                                fontSize = 10.sp
-                            )
-                        )
-                    }
-                }
-
-                HorizontalDivider(
-                    modifier = Modifier.fillMaxWidth(),
-                    thickness = 1.dp,
-                    color = MaterialTheme.colorScheme.outline
-                )
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.star),
-                        contentDescription = "Rating",
-                        tint = MaterialTheme.colorScheme.primary,
-                        modifier = Modifier.size(15.dp)
-                    )
-                    Text(
-                        text = "${item.rating}",
-                        color = MaterialTheme.colorScheme.primary
-                    )
-                    Text(
-                        text = " • ${item.category}",
-                        style = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onSurfaceVariant)
-                    )
-                }
-            }
-
-            Spacer(modifier = Modifier.width(16.dp))
-
-            AsyncImage(
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data(item.imageUrl)
-                    .crossfade(true)
-                    .placeholder(R.drawable.profile_image_fallback)
-                    .build(),
-                contentDescription = item.name,
-                modifier = Modifier
-                    .size(48.dp)
-                    .clip(CircleShape),
-                contentScale = ContentScale.Crop
-            )
-        }
-    }
-}
-
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
