304ffd8f2609f73e98a69523d09e28f82f389218
Dev-Luigy <luigygabriel18a@gmail.com>
2025-02-17 21:03:49 -0300

    feat: Created LocalDateTime and LocalTime DTO converters.

--- HandleService/app/src/main/java/br/com/handleservice/util/LocalTimeAdapter.kt
+++ HandleService/app/src/main/java/br/com/handleservice/util/LocalTimeAdapter.kt
@@ -1,4 +1,25 @@
 package br.com.handleservice.util
 
-class LocalTimeAdapter {
+import com.google.gson.*
+import java.lang.reflect.Type
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+class LocalTimeAdapter : JsonDeserializer<LocalTime>, JsonSerializer<LocalTime> {
+    override fun deserialize(
+        json: JsonElement,
+        typeOfT: Type,
+        context: JsonDeserializationContext
+    ): LocalTime {
+        return OffsetDateTime.parse(json.asString, DateTimeFormatter.ISO_OFFSET_DATE_TIME).toLocalTime()
+    }
+
+    override fun serialize(
+        src: LocalTime,
+        typeOfSrc: Type,
+        context: JsonSerializationContext
+    ): JsonElement {
+        return JsonPrimitive(src.toString()) // Ou utilize um DateTimeFormatter customizado
+    }
 }
